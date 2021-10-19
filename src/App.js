691dfb02061300feded53a493f062b165d27e629
Rae <jueian0822@gmail.com>
2021-10-19 14:10:31 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '@atlaskit/css-reset';
+import { DragDropContext } from 'react-beautiful-dnd';
+import initialData from './initial-data';
+import Column from './column';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Container = styled.div`
+  display: flex;
+`;
+
+class App extends React.Component {
+  state = initialData;
+
+  //儲存移動後的new arry
+  onDragEnd = result => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const start = this.state.columns[source.droppableId];
+    const finish = this.state.columns[destination.droppableId];
+
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds,
+      };
+
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      this.setState(newState);
+      return;
+    }
+
+    // Moving from one list to another
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds,
+    };
+
+    const finishTaskIds = Array.from(finish.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
+      },
+    };
+    this.setState(newState);
+  };
+
+
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Container>
+          {this.state.columnOrder.map(columnId => {
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+            return <Column key={column.id} column={column} tasks={tasks} />;
+          })}
+        </Container>
+      </DragDropContext>
+
+    );
+  }
 }
+
+
+
 
 export default App;
---
+++ src/App2 copy.js
@@ -0,0 +1,223 @@
+import React from 'react';
+import styled from 'styled-components';
+import '@atlaskit/css-reset';
+import {
+  GridContextProvider,
+  GridDropZone,
+  GridItem,
+  swap,
+  move
+} from "react-grid-dnd";
+import "./style.css";
+
+const Container = styled.div`
+  width: 220px;
+  position: relative;
+  height: 240px;
+  &:hover :first-child {
+    display: block;
+  }
+`;
+
+const Dropzone = styled.div`
+  flex: 1;
+  height: 100vh;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+  display: flex;
+`;
+
+// const GridDropZone = styled.div`
+//   flex: 1;
+//   height: 100vh;
+//   border: 1px solid rgba(0, 0, 0, 0.1);
+//   border-radius: 1rem;
+//   display: flex;
+// `;
+
+const DropLeft = styled(Dropzone)`
+  margin-right: 10px;
+  height: 74px;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+
+
+const DropRight = styled(Dropzone)`
+  height: 95vh;
+  max-width: 320px;
+`;
+
+
+function App2() {
+  const [items, setItems] = React.useState({
+    left: [
+      { id: 1, name: "ben" },
+      { id: 2, name: "joe" },
+      { id: 3, name: "jason" },
+      { id: 4, name: "chris" },
+      { id: 5, name: "heather" },
+
+
+    ],
+    left2: [
+      { id: 6, name: "Richard" },
+      { id: 7, name: "george" },
+      { id: 8, name: "rupert" },
+      { id: 9, name: "alice" },
+      { id: 10, name: "katherine" },
+      { id: 11, name: "H1" },
+      { id: 12, name: "H2" },
+      { id: 13, name: "H3" },
+      { id: 14, name: "H4" },
+      { id: 15, name: "H5" },
+    ],
+    left3: [
+
+    ],
+    left4: [
+
+    ],
+    right: [
+      { id: 16, name: "王小明" },
+      { id: 17, name: "ben" },
+      { id: 18, name: "joe" },
+      { id: 19, name: "jason" },
+      { id: 20, name: "chris" },
+      { id: 21, name: "heather" },
+      { id: 22, name: "Richard" },
+      { id: 23, name: "george" },
+      { id: 24, name: "rupert" },
+      { id: 25, name: "alice" },
+      { id: 26, name: "katherine" },
+      { id: 27, name: "H6" },
+      { id: 28, name: "ben" },
+      { id: 29, name: "joe" },
+      { id: 30, name: "jason" },
+      { id: 31, name: "chris" },
+      { id: 32, name: "heather" },
+      { id: 33, name: "Richard" },
+      { id: 34, name: "george" },
+      { id: 35, name: "rupert" },
+      { id: 36, name: "alice" },
+      { id: 37, name: "katherine" },
+      { id: 38, name: "ben" },
+      { id: 39, name: "joe" },
+      { id: 40, name: "jason" },
+
+
+    ]
+  });
+
+  function onChange(sourceId, sourceIndex, targetIndex, targetId) {
+    if (targetId) {
+      const result = move(
+        items[sourceId],
+        items[targetId],
+        sourceIndex,
+        targetIndex
+      );
+      return setItems({
+        ...items,
+        [sourceId]: result[0],
+        [targetId]: result[1]
+      });
+    }
+
+    const result = swap(items[sourceId], sourceIndex, targetIndex);
+    return setItems({
+      ...items,
+      [sourceId]: result
+    });
+  }
+
+  return (
+    <Container>
+      <GridContextProvider onChange={onChange}>
+
+        <GridDropZone
+          className="dropzone left"
+          id="left"
+          boxesPerRow={10}
+          rowHeight={70}
+        >
+          <DropLeft>
+            {items.left.map(item => (
+              <GridItem key={item.id}>
+                <div className="grid-item">
+                  <div className="grid-item-content">
+                    {item.name}
+                  </div>
+                </div>
+              </GridItem>
+            ))}
+          </DropLeft>
+        </GridDropZone>
+        <GridDropZone
+          className="dropzone left left2"
+          id="left2"
+          boxesPerRow={10}
+          rowHeight={70}
+        >
+          <DropLeft>
+            {items.left2.map(item => (
+              <GridItem key={item.id}>
+                <div className="grid-item">
+                  <div className="grid-item-content">
+                    {item.name}
+                  </div>
+                </div>
+              </GridItem>
+            ))}
+          </DropLeft>
+        </GridDropZone>
+        <GridDropZone
+          className="dropzone left left3"
+          id="left3"
+          boxesPerRow={10}
+          rowHeight={70}
+        >
+          <DropLeft>
+            {items.left3.map(item => (
+              <GridItem key={item.id}>
+                <div className="grid-item">
+                  <div className="grid-item-content">
+                    {item.name}
+                  </div>
+                </div>
+              </GridItem>
+            ))}
+          </DropLeft>
+        </GridDropZone>
+
+        <GridDropZone
+          className="dropzone right"
+          id="right"
+          boxesPerRow={4}
+          rowHeight={70}
+        >
+          <DropRight>
+            {items.right.map(item => (
+              <GridItem key={item.id}>
+                <div className="grid-item">
+                  <div className="grid-item-content">
+                    {item.name}
+                  </div>
+                </div>
+              </GridItem>
+            ))}
+          </DropRight>
+        </GridDropZone>
+      </GridContextProvider>
+    </Container>
+  );
+}
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
+
+
+
+export default App2;
